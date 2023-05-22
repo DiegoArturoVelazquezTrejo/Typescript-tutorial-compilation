1b1e6fdd39eac5717fb3a21b94bffb92df0d2d68
velazquez@ciencias.unam.mx <velazquez@ciencias.unam.mx>
2023-05-22 17:39:45 -0600

    Restapi with typescript (42:08)

---
+++ 07-resapi/nestjs-api-tutorial/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+})
+
+// Main module: documentation can be found here (https://docs.nestjs.com/modules)
+export class AppModule {}
